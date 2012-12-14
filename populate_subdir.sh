cb96fd05a6d52baf9cff41cf46736c376853aaa0
Stephen Childs <schilds@ipre-epri.ca>
2012-12-14 06:22:45 -0500

    Populate subdir
    * Grabs subdir.

---
+++ populate_subdir.sh
@@ -0,0 +1,14 @@
+# /bin/bash
+mkdir ado/
+mkdir data/
+mkdir data/output
+mkdir data/log
+mkdir data/source
+mkdir data/user
+mkdir data/work
+mkdir do/
+mkdir estimates/
+mkdir labels/
+mkdir locals/
+mkdir locals/user/
+mkdir user/
